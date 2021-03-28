3cda66013d0f574c6c80ecc656ec2fab07f38b92
Wilamy <wilamy.ribeiroa@gmail.com>
2021-03-28 19:20:18 -0300

    '5'

--- app/settings.py
+++ app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '53a5&9!^xqe@0*rk#_ika@id_6-e=xjn7mhgxr$^sprl-+7so('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://app-teologia.herokuapp.com/', 'http://127.0.0.1:8000/']
 
 
 # Application definition
